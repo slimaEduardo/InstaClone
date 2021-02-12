e46987cb5716aedc970157782501987c46add182
eduardo0lima <55849785+eduardo0lima@users.noreply.github.com>
2021-02-12 14:29:45 -0300

    Controlling the registration form and creating the user model

--- src/app/acesso/cadastro/cadastro.component.ts
+++ src/app/acesso/cadastro/cadastro.component.ts
@@ -1,4 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Usuario } from 'src/app/usuario.model';
+
 
 @Component({
   selector: 'app-cadastro',
@@ -9,6 +12,12 @@ export class CadastroComponent implements OnInit {
 
   @Output() public exibirPainel: EventEmitter<string> = new EventEmitter<string>()
 
+  public formulario: FormGroup = new FormGroup({
+    'email': new FormControl(null),
+    'nome_completo': new FormControl(null),
+    'nome_usuario': new FormControl(null),
+    'senha': new FormControl(null)
+  })
   constructor() { }
 
   ngOnInit(): void {
@@ -16,5 +25,16 @@ export class CadastroComponent implements OnInit {
 
   public exbirPainelLogin(): void {
     this.exibirPainel.emit('login')
+  }
+
+  public cadastrarUsuario(): void {
+    console.log(this.formulario)
+    let usuario: Usuario = new Usuario(
+      this.formulario.value.email,
+      this.formulario.value.nome_completo,
+      this.formulario.value.nome_usuario,
+      this.formulario.value.senha
+    )
+    console.log(usuario)
   }
 }
