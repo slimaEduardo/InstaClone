c85b61434e25fefec7bbdecc87ac28f44d3499d6
eduardo0lima <55849785+eduardo0lima@users.noreply.github.com>
2021-02-14 15:35:01 -0300

    Redirecting to the root path if user is not

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -49,6 +49,9 @@ export class Autenticacao{
         if(this.token_id === undefined && localStorage.getItem('idToken') != null){
             this.token_id = localStorage.getItem('idToken')
         }
+        if(this.token_id === undefined){
+            this.router.navigate(['/'])
+        }
          return this.token_id !== undefined
     }
 
