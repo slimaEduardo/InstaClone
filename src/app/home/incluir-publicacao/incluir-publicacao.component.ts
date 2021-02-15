08f655875e57566b41826d41943249ea6228e645
eduardo0lima <55849785+eduardo0lima@users.noreply.github.com>
2021-02-15 20:10:39 -0300

    Adjusting the modal of inclusion of publications

--- src/app/home/incluir-publicacao/incluir-publicacao.component.ts
+++ src/app/home/incluir-publicacao/incluir-publicacao.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-incluir-publicacao',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IncluirPublicacaoComponent implements OnInit {
 
+  public formulario: FormGroup = new FormGroup({
+    'titulo': new FormControl(null)
+  })
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public publicar(): void {
+    console.log('Chegamos até aqui!')
+  }
 }
