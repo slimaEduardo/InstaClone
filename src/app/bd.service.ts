8e4699fa900f46b36c7e5ae2e3d2e363d75e9ccf
eduardo0lima <55849785+eduardo0lima@users.noreply.github.com>
2021-02-16 20:20:50 -0300

    Controlling upload progress

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -5,9 +5,22 @@ export class Bd {
         console.log(publicacao)
 
         let nomeImagem = Date.now()
-
         firebase.storage().ref()
-        .child(`imagens/${nomeImagem}`).put(publicacao.imagem)
+        .child(`imagens/${nomeImagem}`)
+        .put(publicacao.imagem)
+        .on(firebase.storage.TaskEvent.STATE_CHANGED,
+            /* Evento on() armazena os eventos
+        da tarefa de upload pro storage do firebase*/
+            (snapshot: any) => {
+                //aqui iremos acompanhar o progresso do upload
+                console.log(snapshot)
+            },
+            (error) => {
+             console.log(error)   
+            },
+            ()=>{
+                console.log("upload completo")
+            })
        /*  firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
         .push({titulo: publicacao.titulo}) */
         
