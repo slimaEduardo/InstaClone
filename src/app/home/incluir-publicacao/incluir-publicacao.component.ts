197e2e96cda7f9fadca6c73b061875a4d4548aed
eduardo0lima <55849785+eduardo0lima@users.noreply.github.com>
2021-02-16 20:08:13 -0300

    Upload de arquivos para o Firebase Storage

--- src/app/home/incluir-publicacao/incluir-publicacao.component.ts
+++ src/app/home/incluir-publicacao/incluir-publicacao.component.ts
@@ -11,6 +11,8 @@ import { Bd } from 'src/app/bd.service';
 export class IncluirPublicacaoComponent implements OnInit {
 
   public email: string
+  public imagem: any
+
   public formulario: FormGroup = new FormGroup({
     'titulo': new FormControl(null)
   })
@@ -26,11 +28,12 @@ export class IncluirPublicacaoComponent implements OnInit {
   public publicar(): void {
     this.bd.publicar({
       email: this.email,
-      titulo: this.formulario.value.titulo
+      titulo: this.formulario.value.titulo,
+      imagem: this.imagem[0]
     })
   }
 
   public preparaImagemUpload(event: Event): void{
-    console.log((<HTMLInputElement>event.target).files)
+   this.imagem = (<HTMLInputElement>event.target).files
   }
 }
